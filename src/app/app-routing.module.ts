877ba5e4b35bf64a92524e9b3bd5521bfefef7f8
Nikola Mitrovic <nikolazemamitrovic@gmail.com>
2019-08-20 06:56:35 +0200

    Header navigation.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NewsPageComponent } from './modules/news/pages/news-page/news-page.component';
+import { CategoriesPageComponent } from './modules/news/pages/categories-page/categories-page.component';
+import { SearchPageComponent } from './modules/news/pages/search-page/search-page.component';
 
 
 const routes: Routes = [
 	{
-		path: '',
+		path: 'top-news',
 		component: NewsPageComponent
-	}
+	},
+	{
+		path: 'categories',
+		component: CategoriesPageComponent
+	},
+	{
+		path: 'search',
+		component: SearchPageComponent
+	},
+	{
+		path: '**',
+		redirectTo: 'top-news'
+	},
 ];
 
 @NgModule({
--- src/app/modules/news/news.module.ts
+++ src/app/modules/news/news.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewsPageComponent } from './pages/news-page/news-page.component';
-
+import { CategoriesPageComponent } from './pages/categories-page/categories-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 
 @NgModule({
-	declarations: [NewsPageComponent],
+	declarations: [NewsPageComponent, CategoriesPageComponent, SearchPageComponent],
 	imports: [
 		CommonModule
 	]
